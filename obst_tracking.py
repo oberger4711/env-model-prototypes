Fix variance / std deviation confusion.

obst_tracking.py
@@ -5,8 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DELTA_T = 0.1
-NOISE_VARIANCE = 0.02
-N_MEASUREMENTS = 60
+MEASUREMENT_VARIANCE = 0.05
+MEASUREMENT_STD_DEV = math.sqrt(MEASUREMENT_VARIANCE)
+N_MEASUREMENTS = 80
 LOST_MEASUREMENTS = [30, 50] # Predict only in this interval in the form [t_start, t_end]
 #LOST_MEASUREMENTS = [] # No lost measurements
 
@@ -23,7 +24,7 @@ def spacedmarks(x, y, Nmarks, data_ratio=None):
     return np.array([markx, marky])
 
 # Lane
-t = np.linspace(0, 6, 50)
+t = np.linspace(0, 8, 50)
 points_lane_t = spacedmarks(t, np.sin(t), N_MEASUREMENTS)
 points_lane = np.array(points_lane_t).T
 steps = np.zeros((points_lane.shape[0], 2))
@@ -31,7 +32,7 @@ steps[1:, ] = points_lane[1:, :] - points_lane[:-1, :]
 steps[0,] = steps[1,]
 step_lengths = np.linalg.norm(steps, axis=1)
 vs_true = step_lengths / DELTA_T
-zs = points_lane + np.random.normal(0, math.sqrt(NOISE_VARIANCE), points_lane.shape)
+zs = points_lane + np.random.normal(0, MEASUREMENT_STD_DEV, points_lane.shape)
 
 def get_nearest_direction(x_k, points_lane):
     ds = np.linalg.norm(points_lane - x_k[:2], axis=1)
@@ -76,20 +77,21 @@ def filter(x_k, p_k, z_k, r, points_lane):
     h_k_t = h.T
     # Compute Kalman gain.
     k_k = np.matmul(np.matmul(p_k, h_k_t), np.linalg.inv(np.matmul(np.matmul(h, p_k), h_k_t) + r))
-    print("Kalman Gain: {}".format(k_k.T))
+    print("Kalman Gain:\n {}".format(k_k))
     # Correct state.
     residual = (z_k - np.matmul(h, x_k))
     print(residual)
     x_k = x_k + np.matmul(k_k, residual)
     # Correct covariance.
     p_k = np.matmul((np.eye(3) - np.matmul(k_k, h)), p_k)
+    print("State Covariance:\n {}".format(p_k))
     return x_k, p_k
 
 # KF initialization
-x_k = np.array([0, 0, 0.5]) # p_x, p_y, v_parallel
+x_k = np.array([0, 0, 1.5]) # p_x, p_y, v_parallel
 p_k = np.eye(3) * 10
-r = np.eye(2) * NOISE_VARIANCE
-variance_a = 0.3
+r = np.eye(2) * MEASUREMENT_VARIANCE
+variance_a = 0.6
 
 # Run KF
 xs = np.zeros((zs.shape[0], 3))
@@ -110,7 +112,7 @@ f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 ax1.set_title("Position")
 ax1.plot(points_lane_t[0, :], points_lane_t[1, :], "+-", label="truth")
 ax1.plot(zs.T[0], zs.T[1], "+", label="measurements")
-ax1.plot(xs.T[0], xs.T[1], "+-", label="estimated")
+ax1.errorbar(xs.T[0], xs.T[1], yerr=np.sqrt(ps[:, 1, 1]), fmt="+-", label="estimated")
 if len(LOST_MEASUREMENTS) != 2 or LOST_MEASUREMENTS[0] > k or LOST_MEASUREMENTS[1] < k:
     ax1.plot(xs.T[0, LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1], xs.T[1, LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1], "rx-", linewidth=3, label="prediction_only")
 ax1.legend()
@@ -121,7 +123,7 @@ ax2.errorbar(xs.T[0], xs.T[2], yerr=ps[:, 2, 2], fmt="+-", label="estimated")
 if len(LOST_MEASUREMENTS) != 2 or LOST_MEASUREMENTS[0] > k or LOST_MEASUREMENTS[1] < k:
     xs_pred = xs.T[0, LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1]
     vs_pred = xs.T[2, LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1]
-    err_pred = ps[LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1, 2, 2] **2
+    err_pred = np.sqrt(ps[LOST_MEASUREMENTS[0]:LOST_MEASUREMENTS[1]+1, 2, 2])
     ax2.errorbar(xs_pred, vs_pred, yerr=err_pred, fmt="rx-", linewidth=1, label="prediction_only")
 ax2.set_ylim(1.5 * min(0, np.min(vs_true)), 1.5 * max(0, np.max(vs_true)))
 ax2.legend()